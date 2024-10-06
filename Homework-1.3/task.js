d914eb04d5ab433d3bb4c2093e4b0175387651f1
user <dvbelousov@mail.ru>
2024-10-06 17:28:19 +0300

    first commit

---
+++ Homework-1.3/task.js
@@ -0,0 +1,12 @@
+
+const deadHole = document.getElemetById("dead");
+counter = 0;
+deadHole.onclick = function(){
+counter ++;
+deadHole.textContent = + counter;
+}
+const holeFirst = document.getElemetById("hole1");
+for(let i = 0; i < holeFirst.length; i++){
+	if(hole.className.includes('hole_has-mole')){
+		return deadHole.textContent = + counter;
+	}
